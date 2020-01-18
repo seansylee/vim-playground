235545b075133b44af7b05bee03509400aaa8ff3
seansylee <seanlee014@gmail.com>
2020-01-17 23:06:13 -0800

    Initial set up

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="header">Interactive VimTutor</h2>
+      <div className="description">An Interactive clone of <a href="http://www2.geog.ucl.ac.uk/~plewis/teaching/unix/vimtutor">vimtutor</a></div> 
+      <code>
+        $ vimtutor
+      </code>
+    </div>
+  );
+}
+
+export default App;
